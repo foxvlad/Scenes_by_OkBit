a75b87d3afe09905464ab951e2058e647211fbbb
SmarHome <foxvlad@yandex.ru>
2018-12-17 21:32:34 +0300

    tutu

--- templates/scenes_okbit/sc_templates/home_iOS_style/position/center.php
+++ templates/scenes_okbit/sc_templates/home_iOS_style/position/center.php
@@ -1,6 +1,6 @@
 <div class="img-home">
 	<div class="in-home-1">
-		<img src="../../templates/scene_okbit/sc_templates/home_iOS_style/images/logo_M_O.png" style="float:left;">
+		<img src="../../templates/scenes_okbit/sc_templates/home_iOS_style/images/logo_M_O.png" style="float:left;">
 	</div>
 	<div class="in-home-2">
 		<div class="time-home">
