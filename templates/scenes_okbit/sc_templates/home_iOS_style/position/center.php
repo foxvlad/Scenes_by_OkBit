f1064b39de7c4cd470af1b4da61463aee08aaaa0
SmarHome <foxvlad@yandex.ru>
2018-12-22 18:38:20 +0300

    state light

--- templates/scenes_okbit/sc_templates/home_iOS_style/position/center.php
+++ templates/scenes_okbit/sc_templates/home_iOS_style/position/center.php
@@ -5,6 +5,11 @@
 <div class="img-home">
 	<div class="in-home-1">
 		<img src="../../templates/scenes_okbit/sc_templates/home_iOS_style/images/logo_M_O.png" style="float:left;">
+		<div class="ico-stutus">
+			<div class="ico stutus-sys-%System.stateColor%"></div>
+			<div class="ico stutus-com-%Communication.stateColor%"></div>
+			<div class="ico stutus-sec-%Security.stateColor%"></div>			
+		</div>
 	</div>
 	<div class="in-home-2">
 		<div class="time-home">
