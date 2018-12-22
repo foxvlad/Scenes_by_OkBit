9d66ed35571a45b2552db9d878c72430a711cdb4
SmarHome <foxvlad@yandex.ru>
2018-12-22 15:55:32 +0300

    title heade rmini

--- templates/scenes_okbit/sc_templates/home_iOS_style/position/header.php
+++ templates/scenes_okbit/sc_templates/home_iOS_style/position/header.php
@@ -5,6 +5,9 @@
 <div class="top-title">{{TITLE}}</div>
 <!-- / подстановка название сцены -->
 
+<div class="mini-title">Управление</div>
+
+
 <!-- вывод верхнего меню -->
 <div class="top-menu">{{HEADER}}</div>
 <!-- / вывод верхнего меню -->
